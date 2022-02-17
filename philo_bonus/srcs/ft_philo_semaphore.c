ec5f22534dea0fe2086696d7308f617d02eb2db8
Hamza El makhroubi <hel-makh@e2r11p5.1337.ma>
2022-02-17 15:03:18 +0100

    fixed semaphore mode

--- philo_bonus/srcs/ft_philo_semaphore.c
+++ philo_bonus/srcs/ft_philo_semaphore.c
@@ -6,7 +6,7 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 15:23:36 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/02/16 17:23:40 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/02/17 12:07:41 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	ft_close_semaphores(t_vars *vars)
 	sem_close(vars->queue_sem);
 }
 
-int	ft_init_semaphore(sem_t	*sem, char *sem_name, int value)
+int	ft_init_semaphore(sem_t	**sem, char *sem_name, int value)
 {
-	sem = sem_open(sem_name, O_CREAT | O_EXCL, 0644, value);
-	if (sem == SEM_FAILED)
+	*sem = sem_open(sem_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, value);
+	if (*sem == SEM_FAILED)
 	{
 		sem_unlink(sem_name);
-		sem = sem_open(sem_name, O_CREAT | O_EXCL, 0644, value);
-		if (sem == SEM_FAILED)
+		*sem = sem_open(sem_name, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, value);
+		if (*sem == SEM_FAILED)
 			return (0);
 	}
 	return (1);
@@ -37,16 +37,15 @@ int	ft_init_semaphore(sem_t	*sem, char *sem_name, int value)
 
 int	ft_init_semaphores(t_vars *vars)
 {
-	if (!ft_init_semaphore(vars->fork_sem, FORK_SEM, vars->args.philo_count))
+	if (!ft_init_semaphore(&vars->fork_sem, FORK_SEM, vars->args.philo_count))
 		return (0);
-	if (!ft_init_semaphore(vars->state_sem, STATE_SEM, 1))
+	if (!ft_init_semaphore(&vars->state_sem, STATE_SEM, 1))
 	{
 		sem_unlink(FORK_SEM);
 		sem_close(vars->fork_sem);
 		return (0);
 	}
-	if (!ft_init_semaphore(vars->queue_sem, QUEUE_SEM,
-			vars->args.philo_count))
+	if (!ft_init_semaphore(&vars->queue_sem, QUEUE_SEM, 0))
 	{
 		sem_unlink(FORK_SEM);
 		sem_unlink(STATE_SEM);
