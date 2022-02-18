75ba2729c91a62007221952ca15f1d0c538878ab
Hamza El makhroubi <hel-makh@e1r3p6.1337.ma>
2022-02-18 17:01:58 +0100

    replaced printf by write

--- philo_bonus/srcs/ft_print_state.c
+++ philo_bonus/srcs/ft_print_state.c
@@ -6,16 +6,75 @@
 /*   By: hel-makh <hel-makh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 20:32:12 by hel-makh          #+#    #+#             */
-/*   Updated: 2022/02/16 11:15:15 by hel-makh         ###   ########.fr       */
+/*   Updated: 2022/02/18 12:23:56 by hel-makh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo_bonus.h"
 
+static size_t	ft_strlen(const char *s)
+{
+	size_t	strlen;
+
+	strlen = 0;
+	while (s[strlen] != '\0')
+		strlen ++;
+	return (strlen);
+}
+
+static void	ft_putnbr_fd(int n, int fd)
+{
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		nb = -nb;
+		write(fd, "-", 1);
+	}
+	if (nb > 9)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+	else
+	{
+		nb += '0';
+		write(fd, &nb, 1);
+	}
+}
+
+static char	*ft_strstr(const char *haystack, const char *needle)
+{
+	size_t	i;
+	size_t	j;
+
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			j ++;
+		}
+		i ++;
+	}
+	return (0);
+}
+
 void	ft_print_state(char *state, t_philo *philo)
 {
 	sem_wait(philo->vars->state_sem);
-	printf("%ld %d %s\n", ft_get_current_time(philo->vars),
-		philo->id + 1, state);
-	sem_post(philo->vars->state_sem);
+	ft_putnbr_fd(ft_get_current_time(philo->vars), STDOUT_FILENO);
+	write(STDOUT_FILENO, " ", 1);
+	ft_putnbr_fd(philo->id + 1, STDOUT_FILENO);
+	write(STDOUT_FILENO, " ", 1);
+	write(STDOUT_FILENO, state, ft_strlen(state));
+	write(STDOUT_FILENO, "\n", 1);
+	if (!ft_strstr("died", state))
+		sem_post(philo->vars->state_sem);
 }
